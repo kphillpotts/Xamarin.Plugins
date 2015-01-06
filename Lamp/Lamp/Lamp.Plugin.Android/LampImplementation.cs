3c65ba64f64940c77293c6c1b6725041937de914
Kym Phillpotts <kphillpotts@gmail.com>
2015-01-06 23:08:39 +1000

    Fixed issue with Nexus 5 lamp

--- Lamp/Lamp/Lamp.Plugin.Android/LampImplementation.cs
+++ Lamp/Lamp/Lamp.Plugin.Android/LampImplementation.cs
@@ -5,6 +5,15 @@ using Android.OS;
 using Lamp.Plugin.Abstractions;
 using System;
 using Debug = System.Diagnostics.Debug;
+using Camera = Android.Hardware.Camera;
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Java.IO;
 
 
 namespace Lamp.Plugin
@@ -14,6 +23,8 @@ namespace Lamp.Plugin
   /// </summary>
   public class LampImplementation : ILamp
   {
+      private Camera camera;
+
       /// <summary>
       /// Turn the lamp on
       /// </summary>
@@ -23,7 +34,9 @@ namespace Lamp.Plugin
           // http://forums.xamarin.com/discussion/24237/camera-led-or-flash
           // http://stackoverflow.com/questions/5503480/use-camera-flashlight-in-android?rq=1
 
-          var camera = Camera.Open();
+          if (camera == null)
+              camera = Camera.Open();
+
           if (camera == null)
           {
               Debug.WriteLine("Camera failed to initialize");
@@ -46,6 +59,19 @@ namespace Lamp.Plugin
               p.FlashMode = flashMode;
               camera.SetParameters(p);
           }
+
+          camera.StartPreview();
+
+          // nexus 5 fix here: http://stackoverflow.com/questions/21417332/nexus-5-4-4-2-flashlight-led-not-turning-on
+          try
+          {
+              camera.SetPreviewTexture(new SurfaceTexture(0));
+          }
+          catch (IOException ex)
+          {
+              // Ignore
+          }
+
       }
 
 
@@ -54,7 +80,9 @@ namespace Lamp.Plugin
       /// </summary>
       public void TurnOff()
       {
-          var camera = Camera.Open();
+          if (camera == null)
+              camera = Camera.Open();
+
           if (camera == null)
           {
               Debug.WriteLine("Camera failed to initialize");
@@ -69,8 +97,6 @@ namespace Lamp.Plugin
 
           var flashMode = string.Empty;
 
-          //if (on)
-          //{
           if (supportedFlashModes.Contains(Android.Hardware.Camera.Parameters.FlashModeTorch))
               flashMode = Android.Hardware.Camera.Parameters.FlashModeOff;
 
